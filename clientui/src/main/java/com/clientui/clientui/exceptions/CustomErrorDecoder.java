38afed343172af9050558766a7a5c7dda80e2317
delaval-htps <delaval.htps@gmail.com>
2023-07-13 06:12:53 +0000

    update feign error decoder
    add property "server.error.include-message" to always display error message
    from client feign and keep propagading erro message
    
    add property spring.h2.console.settings.web-allow-others to allow access
    to h2-console in browser

--- clientui/src/main/java/com/clientui/clientui/exceptions/CustomErrorDecoder.java
+++ clientui/src/main/java/com/clientui/clientui/exceptions/CustomErrorDecoder.java
@@ -1,18 +1,60 @@
 package com.clientui.clientui.exceptions;
 
+import java.io.IOException;
+import java.io.InputStream;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import feign.Response;
 import feign.codec.ErrorDecoder;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
 public class CustomErrorDecoder implements ErrorDecoder {
     private final ErrorDecoder defaultErrorDecoder = new Default();
 
     @Override
     public Exception decode(String methodKey, Response response) {
-       // if (response.status() == 400) {
-            return new ProductBadRequestException("requête incorrecte");
-        //}
+       
+        String initialErrorMessage = null;
 
-        //return defaultErrorDecoder.decode(methodKey, response);
+        try (InputStream bodyIs = response.body().asInputStream()) {
+
+            ObjectMapper mapper = new ObjectMapper();
+
+            ExceptionMessage message = mapper.readValue(bodyIs, ExceptionMessage.class);
+            
+            initialErrorMessage = message.getMessage();
+
+        } catch (IOException e) {
+            return new Exception(e.getMessage());
+        }
+
+        switch (response.status()) {
+
+            case 400:
+                return new ProductBadRequestException(
+                        initialErrorMessage != null ? initialErrorMessage : "requête incorrecte");
+            default:
+                return defaultErrorDecoder.decode(methodKey, response);
+        }
+
     }
 
+    @Getter
+    @Setter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @ToString
+    private static class ExceptionMessage {
+        private String timestamp;
+        private int status;
+        private String error;
+        private String message;
+        private String path;
+
+    }
 }
