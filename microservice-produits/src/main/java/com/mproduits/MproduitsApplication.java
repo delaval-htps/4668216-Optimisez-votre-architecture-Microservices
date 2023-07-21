bbaf2fe633a8da13c96a533d4795cbb236eea04e
delaval-htps <delaval.htps@gmail.com>
2023-07-21 15:28:45 +0000

    add client mcommerce-produit to eureka

--- microservice-produits/src/main/java/com/mproduits/MproduitsApplication.java
+++ microservice-produits/src/main/java/com/mproduits/MproduitsApplication.java
@@ -2,8 +2,10 @@ package com.mproduits;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class MproduitsApplication {
 
 	public static void main(String[] args) {
